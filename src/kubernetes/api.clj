f236b7a0c3b3af110ed2f2fedd4dce512c68ec63
Jon Eisen <jon@joneisen.me>
2015-12-03 11:25:46 -0700

    add script to pull swagger doc from kubernetes.io

--- src/kubernetes/api.clj
+++ src/kubernetes/api.clj
@@ -10,18 +10,20 @@
 (defn- url [{:keys [server version]} path]
   (str server "/api/" version path))
 
+(defn parse-response [{:keys [status headers body error]}]
+  (cond
+    error {:success false :error error}
+    (= status 200) {:success true
+                    :body (json/read-str body :key-fn keyword)}
+    :else {:success false :error body}))
+
 (defn request [ctx {:keys [method path]}]
   (let [c (chan)]
     (http/request
      {:url (url ctx path)
       :method method
       :as :text}
-     (fn [{:keys [status headers body error]}]
-       (go (>! c (cond
-                   error {:success false :error error}
-                   (= status 200) {:success true
-                                   :body (json/read-str body :key-fn keyword)}
-                   :else {:success false :error body})))))
+     #(go (>! c (parse-response %))))
     c))
 
 (defn list-nodes [ctx]
