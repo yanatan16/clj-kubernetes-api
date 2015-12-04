6ebb12c08433889e4bc3e9674d39f587e82081fa
Jon Eisen <jon@joneisen.me>
2015-12-04 13:21:15 -0700

    add deploy repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/yanatan16/clj-kubernetes-api"
   :license {:name "MIT"
             :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/data.json "0.2.6"]
