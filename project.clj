cf0af15c40e99fead563bc55bd6021b9fcaf49bf
Jon Eisen <jon@joneisen.me>
2015-12-04 13:22:42 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kubernetes-api "0.1.0"
+(defproject kubernetes-api "0.1.1-SNAPSHOT"
   :description "Kubernetes Client API Library"
   :url "https://github.com/yanatan16/clj-kubernetes-api"
   :license {:name "MIT"
