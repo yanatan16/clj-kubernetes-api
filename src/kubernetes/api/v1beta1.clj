1c4e4c5b5a1c226873ce5f973922c5f7f9e58269
Iago Borges <iago.s.borges@gmail.com>
2017-03-17 17:18:24 -0300

    Remove script to fetch swagger specs from uri

--- src/kubernetes/api/v1beta1.clj
+++ src/kubernetes/api/v1beta1.clj
@@ -1,6 +1,4 @@
 (ns kubernetes.api.v1beta1
-  "Kubernetes v1 API. Auto-generated via macros from swagger documentation.
-   Swagger doc is available from kubernetes.io/swagger-spec/api/v1"
   (:require [kubernetes.api.swagger :as swagger]
             [kubernetes.api.util :as util]))
 
