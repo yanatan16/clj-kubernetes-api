f1eee816983ba779db2233733768f08118079a5b
Iago Borges <iago.s.borges@gmail.com>
2017-03-23 13:56:37 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
-(defproject kubernetes-api "0.1.1-SNAPSHOT"
+(defproject kubernetes-api "0.2.0-SNAPSHOT"
   :description "Kubernetes Client API Library"
   :url "https://github.com/yanatan16/clj-kubernetes-api"
   :license {:name "MIT"
             :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
-  :deploy-repositories [["releases" :clojars]]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/data.json "0.2.6"]
                  [http-kit "2.1.18"]]
+
+  :repositories  [["central"  {:url "http://repo1.maven.org/maven2/" :snapshots false}]
+                  ["clojars"  {:url "https://clojars.org/repo/"}]
+                  ["nu-maven" {:url "s3p://nu-maven/releases/"
+                  :username [:gpg :env/artifacts_aws_access_key_id]
+                  :passphrase [:gpg :env/artifacts_aws_secret_access_key]}]]1
 
   :codox {:namespaces [#"kubernetes\.api\.v\d.*"]}
 
