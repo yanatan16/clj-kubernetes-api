77da1788df26d5edabbecbaef9519b912b282af5
Max Miorim <max.miorim@nubank.com.br>
2017-06-27 18:16:31 -0300

    Update s3-wagon-private

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT"
             :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
 
-  :plugins [[s3-wagon-private "1.2.0"]]
+  :plugins [[s3-wagon-private "1.3.0"]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
@@ -13,9 +13,7 @@
 
   :repositories  [["central"  {:url "http://repo1.maven.org/maven2/" :snapshots false}]
                   ["clojars"  {:url "https://clojars.org/repo/"}]
-                  ["nu-maven" {:url "s3p://nu-maven/releases/"
-                  :username [:gpg :env/artifacts_aws_access_key_id]
-                  :passphrase [:gpg :env/artifacts_aws_secret_access_key]}]]
+                  ["nu-maven" {:url "s3p://nu-maven/releases/"}]]
 
   :codox {:namespaces [#"kubernetes\.api\.v\d.*"]}
 
