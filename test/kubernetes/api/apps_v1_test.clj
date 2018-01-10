8ac0e2b8f9db1ed61a7486f7d0186b272d9533d6
Renan Capaverde <renan.capaverde@nubank.com.br>
2018-01-10 17:15:33 -0200

    Upgrade clj-kubernetes so we can use k8s 1.9
    Also:
    - Cleaned-up tests so they pass
    - Updated clojure to 1.9 and dependencies
    - Updated README so we can run minikube with k8s on 1.9.1

--- test/kubernetes/api/apps_v1_test.clj
+++ test/kubernetes/api/apps_v1_test.clj
@@ -1,21 +1,21 @@
-(ns kubernetes.api.apps-v1beta2-test
+(ns kubernetes.api.apps-v1-test
   (:require [clojure.test :refer :all]
             [clojure.string :as str]
             [clojure.core.async :refer [<!!] :as async]
             [kubernetes.api.v1 :as v1]
-            [kubernetes.api.apps-v1beta2 :as a-v1beta2]))
+            [kubernetes.api.apps-v1 :as a-v1]))
 
 (defn random-name []
   (->> (repeatedly 10 #(rand-int 26))
        (map #(nth (char-array "abcdefghijklmnopqrstuvwxyz") %))
        (str/join "")))
 
-(def ctx (a-v1beta2/make-context "http://localhost:8080"))
+(def ctx (a-v1/make-context "http://localhost:8080"))
 (def tns (random-name))
 (def stateful-set-name (random-name))
 
 (def nsopt {:namespace tns})
-(def stateful-set {:apiVersion "apps/v1beta2"
+(def stateful-set {:apiVersion "apps/v1"
                    :kind       "StatefulSet"
                    :metadata   {:name stateful-set-name}
                    :spec       {:selector {:matchLabels {:service "service"}}
@@ -33,18 +33,21 @@
 
 (deftest stateful-set-test
   (testing "creation of stateful-sets"
-    (let [{:keys[kind metadata]} (<!! (a-v1beta2/create-namespaced-stateful-set ctx stateful-set nsopt))]
+    (let [{:keys[kind metadata]} (<!! (a-v1/create-namespaced-stateful-set ctx stateful-set nsopt))]
       (is (= kind "StatefulSet"))
       (is (= (:name metadata) stateful-set-name))))
 
   (testing "listing stateful-sets"
-    (let [stateful-sets (<!! (a-v1beta2/list-namespaced-stateful-set ctx nsopt))]
+    (let [stateful-sets (<!! (a-v1/list-namespaced-stateful-set ctx nsopt))]
       (is (= stateful-set-name (-> stateful-sets :items first :metadata :name)))
       (is (= "StatefulSetList" (:kind stateful-sets)))))
 
   (testing "reading single stateful-set"
-    (let [{:keys[kind metadata]} (<!! (a-v1beta2/read-namespaced-stateful-set ctx (assoc nsopt :name stateful-set-name)))]
+    (let [{:keys[kind metadata]} (<!! (a-v1/read-namespaced-stateful-set ctx (assoc nsopt :name stateful-set-name)))]
       (is (= kind "StatefulSet"))
-      (is (= (:name metadata) stateful-set-name)))))
+      (is (= (:name metadata) stateful-set-name))))
 
-; Removed deletion test due to resources getting orphan
+  (testing "deleting stateful-set"
+    (let [_ (<!! (a-v1/delete-namespaced-stateful-set ctx {} (assoc nsopt :name stateful-set-name)))
+          {:keys [reason]} (<!! (a-v1/read-namespaced-stateful-set ctx (assoc nsopt :name stateful-set-name)))]
+      (is (= "NotFound" reason)))))
