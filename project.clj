d0f1279f0dff49df2e54a34c8d7e7405d185f062
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-12-21 10:45:56 -0300

    Bump libs, update project.clj

--- project.clj
+++ project.clj
@@ -1,22 +1,20 @@
 (defproject nubank/kubernetes-api "1.6.1"
   :description "Kubernetes Client API Library"
-  :url "https://github.com/yanatan16/clj-kubernetes-api"
+  :url "https://github.com/nubank/clj-kubernetes-api"
   :license {:name "MIT"
-            :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
+            :url  "https://github.com/nubank/clj-kubernetes-api/blob/master/LICENSE"}
 
-  :plugins [[s3-wagon-private "1.3.1"]
-            [lein-ancient "0.6.14"]]
+  :plugins [[lein-ancient "0.7.0"]]
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.2.0"]
-                 [less-awful-ssl "1.0.2"]]
+                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/data.json "2.4.0"]
+                 [http-kit "2.5.3"]
+                 [less-awful-ssl "1.0.6"]]
 
   :repositories  [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
-                  ["clojars"  {:url "https://clojars.org/repo/"}]
-                  ["nu-maven" {:url "s3p://nu-maven/releases/" :snapshots false :sign-releases false}]]
+                  ["clojars"  {:url "https://clojars.org/repo/"}]]
 
   :codox {:namespaces [#"kubernetes\.api\.v\d.*"]}
 
-  :profiles {:dev {:plugins [[lein-codox "0.9.0"]]}})
+  :profiles {:dev {:plugins [[lein-codox "0.10.8"]]}})
